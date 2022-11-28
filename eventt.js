47b65a67b8d93c6000a61140d1e0cd4d6773bcc5
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-11-28 16:58:01 +0100

    Learing events javascript

--- eventt.js
+++ eventt.js
@@ -15,12 +15,52 @@ input.addEventListener("keydown", (e) => {
       console.log("UP!");
   }
 });*/
-
-/* const form = document.querySelector("#shelterForm");
+/* 
+const form = document.querySelector("#shelterForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("SUMMITED THE FORM");
+}); */
+
+//PRACTICE
+
+/* const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(input.value);
+  const catName = input.value;
+  const newLi = document.createElement("li");
+  newLi.innerText = catName;
+  list.append(newLi);
 });
  */
-//PRACTICE
+
+const form = document.querySelector("form");
+const product = document.querySelector("#product");
+const quantity = document.querySelector("#qty");
+const list = document.querySelector("#list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const prod = product.value;
+  const qty = quantity.value;
+  if (prod === "" || qty === "") {
+    alert("Characters cant be empty");
+  } else {
+    const li = document.createElement("li");
+    li.innerText = `${qty} ${prod}`;
+
+    list.append(li);
+  }
+});
+
+function hello() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+}
+
+hello();
